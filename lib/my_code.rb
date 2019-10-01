84cd28ca9f8c7527b276676a8eb3b947118996a7
Kuran V Kumar <kurankumar@gmail.com>
2019-10-01 19:42:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,3 +8,14 @@ def map_to_negativize(source_array)
  end
  return negative_array
 end
+
+
+def map_to_no_change(source_array)
+  i = 0 
+  noChange_array = []
+  while i < source_array.length do
+    noChange_array[i] = source_array[i]
+    i += 1 
+  end
+  return noChange_array
+end
