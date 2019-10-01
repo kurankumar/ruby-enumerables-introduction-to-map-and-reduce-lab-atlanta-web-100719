40c77973b82600a024a582aee59e26d01ef53713
Kuran V Kumar <kurankumar@gmail.com>
2019-10-01 20:01:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -65,7 +65,16 @@ def reduce_to_all_true(source_array)
 end
 
 
-
+def reduce_to_any_true(source_array)
+  i = 0 
+  while i < source_array.length do 
+    if (source_array[i] == true)
+      return true 
+    end
+    i += 1 
+  end
+  return false
+end
 
 
 
