a535e3320fe4ee540ad1abdc257ed22f83811a9e
Kuran V Kumar <kurankumar@gmail.com>
2019-10-01 19:45:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -19,3 +19,14 @@ def map_to_no_change(source_array)
   end
   return noChange_array
 end
+
+
+def map_to_double(source_array)
+  i = 0 
+  doubled_array = []
+  while i < source_array.length do 
+    doubled_array[i] = source_array[i] * 2 
+    i += 1 
+  end
+  return doubled_array
+end
